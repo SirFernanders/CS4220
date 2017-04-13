a4c3548d70cf072452b732e21e3d2f5ed50d11fc
Fernando <fernandomontes91@me.com>
2017-04-12 21:58:50 -0700

    just about done it pulls all the info it need got to change everything to say music instead of tv show

--- Week11/tvmaze-app/client/js/controllers/search.js
+++ Week11/tvmaze-app/client/js/controllers/search.js
@@ -4,9 +4,29 @@ angular
 
         $scope.searchShow = () => {
             TVMazeService.search.query({
-                show: $scope.showname
+                id:"",
+                show: "&q="+$scope.showname,
+                type: "search?type=artist"
             }, (response) => {
-                $scope.results = response
+                $scope.results = response;
+                $scope.albums = null;
+                console.log(response);
+
+            })
+        }
+
+        $scope.searchAlbums = (id,name,image) => {
+            TVMazeService.search.query({
+                id: "/artists/"+id,
+                show: "",
+                type: "albums?album_type=album&market=US"
+            }, (response) => {
+                $scope.artistinfo = {name:name,url:image};
+                $scope.albums = response;
+                $scope.results = null;
+                console.log(response);
+
+
             })
         }
 
