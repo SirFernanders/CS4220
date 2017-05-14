e730c7994f499d9f0033436853358405ec4fba07
Fernando <fernandomontes91@me.com>
2017-05-14 15:10:12 -0700

    added vincents code

--- Final/index.js
+++ Final/index.js
@@ -1,11 +1,8 @@
-/**
- * Created by Fernando on 4/30/17.
- */
-
 const chalk = require('chalk');
 const inquirer = require('inquirer');
 const figlet = require('figlet');
-
+const readline = require('readline');
+const colors = require('colors');
 
 /*******************
  *
@@ -14,74 +11,112 @@ const figlet = require('figlet');
  *
  *
  *******************/
-let convertedTextArt = "", colorOutline = 'blue', colorFiller = 'gray', background = 'bgRed';
 
-const bgColors = { Black: "bgBlack", Red: "bgRed", Green: "bgGreen", Yellow: "bgYellow", Blue: "bgBlue", Magenta : "bgMagenta", Cyan : "bgCyan", White: "bgWhite"};
-const colors = {Black :'black', Red : 'red', Green : 'green', Yellow : 'yellow', Blue : 'blue', Magenta : 'magenta', Cyan : 'cyan', White : 'white', Gray : 'gray'};
+const fernando = ()=> {
+    let convertedTextArt = "", colorOutline = 'blue', colorFiller = 'gray', background = 'bgRed';
 
-const FMstart = ()=>{
-    inquirer.prompt([{
-        type: 'list', name: 'answer', message: `What background color do you want?`, choices: ['Black', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White', 'Gray']
-    }]).then((list)=>{
-        background = list.answer;
+    const bgColors = {
+        Black: "bgBlack",
+        Red: "bgRed",
+        Green: "bgGreen",
+        Yellow: "bgYellow",
+        Blue: "bgBlue",
+        Magenta: "bgMagenta",
+        Cyan: "bgCyan",
+        White: "bgWhite"
+    };
+    const colors = {
+        Black: 'black',
+        Red: 'red',
+        Green: 'green',
+        Yellow: 'yellow',
+        Blue: 'blue',
+        Magenta: 'magenta',
+        Cyan: 'cyan',
+        White: 'white',
+        Gray: 'gray'
+    };
+
+    const inputStart = () => {
         inquirer.prompt([{
-            type: 'list', name: 'answer', message: `What outline color do you want?`, choices: ['Black', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White', 'Gray']
-        }]).then((list)=>{
-            colorOutline = list.answer;
+            type: 'list',
+            name: 'answer',
+            message: `What background color do you want?`,
+            choices: ['Black', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White', 'Gray']
+        }]).then((list) => {
+            background = list.answer;
             inquirer.prompt([{
-                type: 'list', name: 'answer', message: `What filler color do you want?`, choices: ['Black', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White', 'Gray']
-            }]).then((list)=>{
-                colorFiller = list.answer;
-                textInput();
+                type: 'list',
+                name: 'answer',
+                message: `What outline color do you want?`,
+                choices: ['Black', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White', 'Gray']
+            }]).then((list) => {
+                colorOutline = list.answer;
+                inquirer.prompt([{
+                    type: 'list',
+                    name: 'answer',
+                    message: `What filler color do you want?`,
+                    choices: ['Black', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White', 'Gray']
+                }]).then((list) => {
+                    colorFiller = list.answer;
+                    textInput();
+                })
             })
         })
-    })
-};
+    };
 
-const textInput = () =>{
-    inquirer.prompt([{
-        type: 'input', name: 'text', message: 'Input some text to change?'
-    }]).then((list)=>{
-        figlet.text(list.text, {
-            font:'Doh', horizontalLayout: 'default', verticalLayout:'default'
-        }, function (err, data) {
-            textConvert((data+"").split(""));
-        });
-    })
-};
+    const textInput = () => {
+        inquirer.prompt([{
+            type: 'input', name: 'text', message: 'Input some text to change?'
+        }]).then((list) => {
+            figlet.text(list.text, {
+                font: 'Doh', horizontalLayout: 'default', verticalLayout: 'default'
+            }, function (err, data) {
+                textConvert((data + "").split(""));
+            });
+        })
+    };
 //**************************************************************//
 //**************************************************************//
-const textConvert = function (text) {
+    const textConvert = function (text) {
 
-    const chalkBackground = chalk[bgColors[background]];
-    const chalkFiller = chalk[colors[colorFiller]];
-    const chalkOutline = chalk[colors[colorOutline]];
+        const chalkBackground = chalk[bgColors[background]];
+        const chalkFiller = chalk[colors[colorFiller]];
+        const chalkOutline = chalk[colors[colorOutline]];
 
-        text.forEach((element)=>{
-            if("\n"===element){
+        text.forEach((element) => {
+            if ("\n" === element) {
                 convertedTextArt += element;
             }
-            else if(":"===element){
+            else if (":" === element) {
                 convertedTextArt += chalkFiller(chalkBackground(element));
             }
-            else if(" "===element){
+            else if (" " === element) {
                 convertedTextArt += chalkBackground(element);
             }
-            else{
+            else {
                 convertedTextArt += chalkOutline(chalkBackground(element));
             }
 
         });
 
-    console.log(convertedTextArt);
-    inquirer.prompt([{
-        type: 'list', name: 'answer', message: `Would you like to do it again?`, choices: ['yes', 'no']
-    }]).then((list)=>{
-        console.log("\n");
-        if(list.answer==='yes'){FMstart();}
-        else{start();}
-    })
+        console.log(convertedTextArt);
+        inquirer.prompt([{
+            type: 'list', name: 'answer', message: `Would you like to do it again?`, choices: ['yes', 'no']
+        }]).then((list) => {
+            console.log("\n");
+            if (list.answer === 'yes') {
+                inputStart();
+            }
+            else {
+                start();
+            }
+        })
+
+    };
+    inputStart();
 };
+
 
 /*******************
  *
@@ -90,8 +125,6 @@ const textConvert = function (text) {
  *
  *
  *******************/
-
-
 
 const walter = ()=> {
     const ONE_MINUTE_THIRSTY_SECONDS_IN_MILLISECONDS = 90000;
@@ -130,32 +163,189 @@ const walter = ()=> {
 };
 
 
+/*******************
+ *
+ *
+ *  Vincents's Code
+ *
+ *
+ *******************/
+
+const vince = ()=>{
+
+    const fire = chalk.red.bold('Fire ')
+    const water = chalk.blue.bold('Water ')
+    const earth = chalk.green.bold('Earth ')
+
+    let userInput = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    let randomType = getRandomIntInclusive(1, 3)
+
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function type(){
+
+        switch(randomType) {
+            case 1:
+                randomType = fire
+                break;
+            case 2:
+                randomType = water
+                break;
+            case 3:
+                randomType = earth
+                break;
+        }
+
+        return randomType
+    }
+
+    monsterAttack = (type() + 'monster attacks you.\n')
+
+    monsterDie = (type() + 'monster dies.\n')
+
+    youDie = chalk.bold.red('You die immediately afterwards.')
+
+    youLive = chalk.bold.cyan('Congratulations! You live to fight another day.')
+
+
+    console.log(chalk.blue.inverse('You are an adventurer with a tragic backstory\
+ and knows basic magic.\nYou are trying to reach an ultimate goal while facing\
+ enemies along your path.\nYou are aware of the basic elements of battle.'))
+
+    console.log(fire + 'beats ' + earth)
+    console.log(water + 'beats ' + fire)
+    console.log(earth + 'beats ' + water +'\n')
+
+
+    function monster(){
+        console.log(chalk.underline('Today, you encounter a ' + type() + 'monster.'))
+    }
+
+    monster()
+
+    userInput.question(chalk.bold.cyan('How do you attack? \n'), (answer) =>{
+        if (type() == fire ){
+            if (`${answer}` == 'Fire'){
+                console.log('You attacked with ' + fire + '\n' + chalk.dim('It was not effective...\n') + monsterAttack + youDie)
+            }
+            else if (`${answer}` == 'Water'){
+                console.log('You attacked with ' + water + '\n' + chalk.bold('It was very effective!\n') + monsterDie + youLive)
+            }
+            else if (`${answer}` == 'Earth'){
+                console.log('You attacked with ' + earth + '\n' + chalk.dim('It was not effective...\n') + monsterAttack + youDie)
+            }
+            else{
+                console.log('You spout gibberish. ' + fire + 'Monster is unfazed by your words.\n' + monsterAttack + youDie)
+            }
+        }
+        else if (type() == water ){
+            if (`${answer}` == 'Fire'){
+                console.log('You attacked with ' + fire + '\n' + chalk.dim('It was not effective...\n') + monsterAttack + youDie)
+            }
+            else if (`${answer}` == 'Water'){
+                console.log('You attacked with ' + water + '\n' + chalk.dim('It was not effective...\n') + monsterAttack + youDie)
+            }
+            else if (`${answer}` == 'Earth'){
+                console.log('You attacked with ' + earth + '\n' + chalk.bold('It was very effective!\n') + monsterDie + youLive)
+            }
+            else{
+                console.log('You spout gibberish. ' + water + 'Monster is unfazed by your words.\n' + monsterAttack + youDie)
+            }
+        }
+        else {
+            if (`${answer}` == 'Fire'){
+                console.log('You attacked with ' + fire + '\n' + chalk.bold('It was very effective!\n') + monsterDie + youLive)
+            }
+            else if (`${answer}` == 'Water'){
+                console.log('You attacked with ' + water + '\n' + chalk.dim('It was not effective...\n') + monsterAttack + youDie)
+            }
+            else if (`${answer}` == 'Earth'){
+                console.log('You attacked with ' + earth + '\n' + chalk.dim('It was not effective...\n') + monsterAttack + youDie)
+            }
+            else{
+                console.log('You spout gibberish. ' + earth + 'Monster is unfazed by your words.\n' + monsterAttack + youDie)
+            }
+        }
+        userInput.close();
+        start();
+    });
+
+};
+
+
+/*******************
+ *
+ *
+ *  Frank's Code
+ *
+ *
+ *******************/
+
+const frank = ()=>{
+
+};
+
+
+/*******************
+ *
+ *
+ *  Wendy's Code
+ *
+ *
+ *******************/
+
+const wendy = () =>{
+
+};
+
+
+/*******************
+ *
+ *
+ *  Kevin's Code
+ *
+ *
+ *******************/
+
+const kevin = ()=>{
+
+};
+
+
+
 
 
 const start = ()=> {
     inquirer.prompt([{
-        type: 'list', name: 'answer', message: `Demo?`, choices: ['Fernando', 'Walter', 'Frank', 'Wendy', 'Vincent', 'Kevin']
+        type: 'list', name: 'answer', message: `Demo?`, choices: ['Fernando', 'Walter', 'Vincent', 'Frank', 'Wendy', 'Kevin']
     }]).then((list) => {
         if (list.answer === 'Fernando') {
-            FMstart();
+            fernando();
         }
         else if (list.answer === 'Walter') {
             walter();
         }
         else if (list.answer === 'Frank') {
-
+            frank();
         }
         else if (list.answer === 'Wendy') {
-
+            wendy();
         }
         else if (list.answer === 'Vincent') {
-
+            vince();
         }
         else if (list.answer === 'Kevin') {
-
+            kevin();
         }
 
     });
 };
-
 start();
